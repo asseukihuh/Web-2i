2460da7dcf918972e2f093b1bfc49ee0b22a5e8c
Ny-Harena RAKOTOVAO <107315159+asseukihuh@users.noreply.github.com>
2025-03-05 16:55:45 +0100

    djhajhdzahd

---
+++ CTP 2024/script.js
@@ -0,0 +1,314 @@
+var exercices = [
+    {
+        "nom": "Burpees explosifs",
+        "description": "Commencez en position debout, faites une flexion, puis sautez en l'air en tendant vos bras au-dessus de votre t&ecirc;te.",
+        "difficulte": 4
+    },
+    {
+        "nom": "Mountain Climbers",
+        "description": "En position de planche, alternez rapidement le repli des genoux vers la poitrine comme si vous escaladiez une montagne.",
+        "difficulte": 3
+    },
+    {
+        "nom": "Sauts group&eacute;s",
+        "description": "En position accroupie, sautez en l'air en repliant vos genoux vers la poitrine, puis atterrissez doucement.",
+        "difficulte": 4
+    },
+    {
+        "nom": "Fentes saut&eacute;es",
+        "description": "Faites une fente en avant, puis sautez en changeant rapidement de jambe en l'air.",
+        "difficulte": 3
+    },
+    {
+        "nom": "Pompes &agrave; d&eacute;calage",
+        "description": "Faites des pompes, en d&eacute;calant vos mains vers la gauche puis vers la droite &agrave; chaque r&eacute;p&eacute;tition.",
+        "difficulte": 3
+    },
+    {
+        "nom": "Sprints sur place",
+        "description": "Levez les genoux aussi haut que possible en effectuant des sprints sur place.",
+        "difficulte": 2
+    },
+    {
+        "nom": "Planche avec touches de genoux",
+        "description": "En position de planche, touchez alternativement vos genoux avec vos mains.",
+        "difficulte": 3
+    },
+    {
+        "nom": "Squats saut&eacute;s",
+        "description": "Effectuez un squat, puis sautez en l'air en &eacute;tendant vos bras au-dessus de votre t&ecirc;te.",
+        "difficulte": 4
+    },
+    {
+        "nom": "Corde &agrave; sauter",
+        "description": "Faites des sauts &agrave; la corde &agrave; une cadence rapide.",
+        "difficulte": 2
+    },
+    {
+        "nom": "Planche lat&eacute;rale avec &eacute;l&eacute;vation du bras",
+        "description": "En position de planche lat&eacute;rale, levez le bras sup&eacute;rieur vers le plafond, puis revenez &agrave; la position de d&eacute;part.",
+        "difficulte": 3
+    },
+    {
+        "nom": "Ciseaux",
+        "description": "Allongez-vous sur le dos et alternez le levage des jambes en position horizontale.",
+        "difficulte": 2
+    },
+    {
+        "nom": "Fentes avec rotation du tronc",
+        "description": "Effectuez une fente en avant, puis tournez le tronc du c&ocirc;t&eacute; de la jambe avant.",
+        "difficulte": 3
+    },
+    {
+        "nom": "Pompes de planche",
+        "description": "En position de planche, fl&eacute;chissez les coudes pour descendre le corps vers le sol, puis remontez.",
+        "difficulte": 3
+    },
+    {
+        "nom": "Sprint en place avec genoux hauts",
+        "description": "Sprintez sur place en levant les genoux aussi haut que possible.",
+        "difficulte": 2
+    },
+    {
+        "nom": "Sauts lat&eacute;raux",
+        "description": "Sautez lat&eacute;ralement d'un c&ocirc;t&eacute; &agrave; l'autre avec un atterrissage doux.",
+        "difficulte": 3
+    },
+    {
+        "nom": "Planche avec rotation du bras",
+        "description": "En position de planche, levez un bras vers le plafond, puis revenez &agrave; la position de planche.",
+        "difficulte": 3
+    },
+    {
+        "nom": "Squats avec coup de pied lat&eacute;ral",
+        "description": "Effectuez un squat, puis levez la jambe lat&eacute;ralement lors de la remont&eacute;e.",
+        "difficulte": 3
+    },
+    {
+        "nom": "Pompes d'&eacute;paule",
+        "description": "En position de planche, effectuez une pompe tout en alternant le levage des bras.",
+        "difficulte": 4
+    },
+    {
+        "nom": "Fentes invers&eacute;es avec coup de poing",
+        "description": "Effectuez une fente invers&eacute;e, puis effectuez un coup de poing avec le bras avant.",
+        "difficulte": 3
+    },
+    {
+        "nom": "Gainage avec lev&eacute;e des jambes",
+        "description": "En position de planche, levez une jambe &agrave; la fois tout en maintenant une position stable.",
+        "difficulte": 3
+    },
+    {
+        "nom": "Jumping Jacks",
+        "description": "Commencez en position debout, puis sautez en &eacute;cartant les jambes et en levant les bras au-dessus de la t&ecirc;te.",
+        "difficulte": 2
+    },
+    {
+        "nom": "Sprint en mont&eacute;e",
+        "description": "Sprintez en montant des escaliers ou une pente raide pour intensifier l'entra&icirc;nement.",
+        "difficulte": 4
+    },
+    {
+        "nom": "Flexions de jambes",
+        "description": "En position debout, fl&eacute;chissez les jambes comme si vous alliez vous asseoir, puis revenez &agrave; la position initiale.",
+        "difficulte": 2
+    },
+    {
+        "nom": "Pompes plyom&eacute;triques",
+        "description": "Effectuez des pompes en poussant votre corps vers le haut de mani&egrave;re explosive.",
+        "difficulte": 4
+    },
+    {
+        "nom": "Planche avec lev&eacute;e de bras",
+        "description": "En position de planche, levez un bras vers l'avant, puis alternez.",
+        "difficulte": 3
+    },
+    {
+        "nom": "Sprint &agrave; reculons",
+        "description": "Sprintez en reculant pour travailler les muscles de mani&egrave;re diff&eacute;rente.",
+        "difficulte": 3
+    },
+    {
+        "nom": "Jumping Lunges",
+        "description": "Faites une fente en avant, puis sautez en changeant rapidement de jambe en l'air.",
+        "difficulte": 4
+    },
+    {
+        "nom": "Box Jumps",
+        "description": "Sautez sur une bo&icirc;te stable, puis redescendez.",
+        "difficulte": 4
+    },
+    {
+        "nom": "Planche avec rotation du tronc",
+        "description": "En position de planche, tournez le tronc et levez le bras vers le plafond, puis revenez &agrave; la position de planche.",
+        "difficulte": 3
+    },
+    {
+        "nom": "Fentes avec saut vertical",
+        "description": "Effectuez une fente en avant, puis sautez en l'air en changeant rapidement de jambe.",
+        "difficulte": 4
+    },
+    {
+        "nom": "Sprint &agrave; genoux",
+        "description": "Sprintez sur place en position &agrave; genoux.",
+        "difficulte": 3
+    },
+    {
+        "nom": "Pompes diamant",
+        "description": "En position de pompes, placez vos mains sous la poitrine de mani&egrave;re &agrave; former un diamant avec vos pouces et vos index.",
+        "difficulte": 4
+    },
+    {
+        "nom": "Sauts &agrave; la corde &agrave; une jambe",
+        "description": "Faites des sauts &agrave; la corde en levant une jambe &agrave; la fois.",
+        "difficulte": 3
+    },
+    {
+        "nom": "Flexions avec rotation",
+        "description": "En position de pompes, effectuez une rotation du tronc en levant un bras vers le plafond.",
+        "difficulte": 3
+    },
+    {
+        "nom": "Squats avec saut lat&eacute;ral",
+        "description": "Effectuez un squat, puis sautez lat&eacute;ralement en changeant de direction &agrave; chaque r&eacute;p&eacute;tition.",
+        "difficulte": 3
+    },
+    {
+        "nom": "Planche avec lev&eacute;e de jambe",
+        "description": "En position de planche, levez une jambe &agrave; la fois tout en maintenant une position stable.",
+        "difficulte": 3
+    },
+    {
+        "nom": "Sprints &agrave; une jambe",
+        "description": "Sprintez sur place en levant une jambe &agrave; la fois.",
+        "difficulte": 4
+    },
+    {
+        "nom": "Jump Squats",
+        "description": "Effectuez un squat, puis sautez en l'air en &eacute;tendant vos bras au-dessus de votre t&ecirc;te.",
+        "difficulte": 4
+    },
+    {
+        "nom": "Pompes en T",
+        "description": "En position de pompes, &eacute;tendez les bras sur les c&ocirc;t&eacute;s &agrave; la hauteur des &eacute;paules.",
+        "difficulte": 3
+    },
+    {
+        "nom": "Sprint &agrave; genoux avec rotation",
+        "description": "Sprintez sur place en position &agrave; genoux tout en effectuant une rotation du tronc.",
+        "difficulte": 4
+    },
+    {
+        "nom": "Squats avec saut en &eacute;toile",
+        "description": "Effectuez un squat, puis sautez en l'air en &eacute;cartant les bras et les jambes.",
+        "difficulte": 4
+    },
+    {
+        "nom": "Pompes araign&eacute;e",
+        "description": "En position de pompes, ramenez alternativement le genou vers le coude.",
+        "difficulte": 3
+    },
+    {
+        "nom": "Marche sur place",
+        "description": "Levez alternativement les genoux en marchant sur place &agrave; un rythme soutenu.",
+        "difficulte": 1
+    },
+    {
+        "nom": "Talons fesses",
+        "description": "Marchez sur place en levant les talons pour toucher les fesses &agrave; chaque pas.",
+        "difficulte": 1
+    },
+    {
+        "nom": "Mont&eacute;e de genoux",
+        "description": "Marchez sur place en levant les genoux aussi haut que possible &agrave; chaque pas.",
+        "difficulte": 1
+    },
+    {
+        "nom": "Pompes murales",
+        "description": "Effectuez des pompes en utilisant un mur comme support, en pliant et d&eacute;pliant les coudes.",
+        "difficulte": 1
+    },
+    {
+        "nom": "Jumping Jacks modifi&eacute;s",
+        "description": "Effectuez des Jumping Jacks &agrave; un rythme plus lent pour une version plus douce.",
+        "difficulte": 1
+    },
+    {
+        "nom": "Squats de chaise",
+        "description": "Asseyez-vous sur une chaise, puis levez-vous en poussant &agrave; travers les talons.",
+        "difficulte": 1
+    },
+    {
+        "nom": "Planche de genoux",
+        "description": "Adoptez la position de planche, mais en reposant les genoux au sol plut&ocirc;t que les pieds.",
+        "difficulte": 1
+    },
+    {
+        "nom": "Fentes stationnaires",
+        "description": "Effectuez des fentes en avan&ccedil;ant une jambe &agrave; la fois sans faire de pas.",
+        "difficulte": 1
+    },
+    {
+        "nom": "Marche rapide",
+        "description": "Marchez &agrave; un rythme rapide, en balan&ccedil;ant naturellement les bras.",
+        "difficulte": 1
+    },
+    {
+        "nom": "Circuits de bras",
+        "description": "Levez et abaissez alternativement les bras sur le c&ocirc;t&eacute; &agrave; hauteur d'&eacute;paule.",
+        "difficulte": 1
+    }
+];
+
+var divexo1 = document.getElementById("listexo");
+let listdiff = ["diff1","diff2","diff3","diff4"]
+
+
+var searchtext = document.getElementById("rechercher").value;
+
+for(let i = 0; i < exercices.length; i++){
+
+    console.log(exercices[i].description.search(searchtext));
+
+    if(exercices[i].description.search(searchtext) === 0){
+        divexo1.innerHTML+="<div id="+listdiff[exercices[i].difficulte-1]+">"+
+                    "<p id='titrexo'>"+exercices[i].nom+"</p>"+
+                    "<p>"+exercices[i].description+"</p>"+
+                    "<p> Difficulté : "+exercices[i].difficulte+"</p>"
+                    "</div>";
+    }
+}
+
+function enter(event){
+        if(event.key == "Enter"){
+            searchtext = document.getElementById("rechercher").value;
+        }
+
+        divexo1.innerHTML = ""
+
+        for(let i = 0; i < exercices.length; i++){
+
+        console.log(exercices[i].description.search(searchtext));
+
+        if(exercices[i].description.search(searchtext) !== -1 || exercices[i].nom.search(searchtext) !== -1){
+            divexo1.innerHTML+="<div id="+listdiff[exercices[i].difficulte-1]+">"+
+                        "<p id='titrexo'>"+exercices[i].nom+"</p>"+
+                        "<p>"+exercices[i].description+"</p>"+
+                        "<p> Difficulté : "+exercices[i].difficulte+"</p>"
+                        "</div>";
+            }
+        }
+}
+
+var slots = 0;
+var divslots = document.getElementById("slots");
+
+function changeslots(){
+    slots = document.getElementById("exoslots").value;
+    divslots.innerHTML = "";
+
+    for(let i = 0; i<slots;i++){
+        divslots.innerHTML+="<p>caca</p>";
+    }
+}
