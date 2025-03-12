ebf804bbffe13439dea0dd7cc339a6b9653b99e1
Ny-Harena RAKOTOVAO <107315159+asseukihuh@users.noreply.github.com>
2025-03-12 16:49:41 +0100

    Templare
    dsadsa

---
+++ WebTemplate/script.js
@@ -0,0 +1,60 @@
+//JS SCRIPT
+
+var var1 = document.getElementById("IdExample1");
+var var2 = document.getElementById("IdExample2");
+var var3 = document.getElementById("IdExample3");
+var var4 = document.getElementById("IdExample4");
+
+var form1 = document.forms["FormExample1"];
+var form2 = document.forms["FormExample1"];
+
+//JSON EXAMPLE
+
+var JsonExample = [
+    {
+        "nom": "Burpees explosifs",
+        "description": "Commencez en position debout, faites une flexion, puis sautez en l'air en tendant vos bras au-dessus de votre t&ecirc;te.",
+        "difficulte": 4
+    },
+    {
+        "nom": "Mountain Climbers",
+        "description": "En position de planche, alternez rapidement le repli des genoux vers la poitrine comme si vous escaladiez une montagne.",
+        "difficulte": 3
+    },
+    {
+        "nom": "Sauts group&eacute;s",
+        "description": "En position accroupie, sautez en l'air en repliant vos genoux vers la poitrine, puis atterrissez doucement.",
+        "difficulte": 4
+    }
+];
+
+//FUNCTIONS
+
+function main(){
+    return 0;
+}
+
+function main2(){
+    return 0;
+}
+
+function main3(){
+    return 0;
+}
+
+function main4(){
+    return 0;
+}
+
+// STRUCTURES DE BOUCLES ETC
+
+for(let i = 0; i < ?? ; i++){
+    //faire jsp
+}
+
+if (Condition){
+    //faire jsp
+} else {
+    //faire jsp
+}
+
